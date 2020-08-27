f877866663c725bd0068dc5220867a9820e9aa4d
Ale Diaz <diazale94@outlook.com>
2020-08-27 20:18:30 -0300

    clientes

--- resources/views/clientes.blade.php
+++ resources/views/clientes.blade.php
@@ -13,8 +13,21 @@
         </div>
       </div>
 
+
+
       <div class="row mt-3">
         <div class="col-12">
+        <table class="table">
+  	  	  <thead>
+  	  	  	  <tr>
+  	  	  		<th bgcolor="#FF3F2C"> Id </th>
+  	  	  		<th bgcolor="#FF3F2C"> Nombre </th>
+              <th bgcolor="#FF3F2C"> Apellido </th>
+              <th bgcolor="#FF3F2C"> Telefono </th>
+              <th bgcolor="#FF3F2C"> Domicilio </th>
+              <th bgcolor="#FF3F2C"> Acciones</th>
+  	  	  	  </tr>
+  	  	  </thead>          
         </div>
       </div>
   </div>
