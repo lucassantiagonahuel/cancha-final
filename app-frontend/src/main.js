e3eb05afd41ede3b00c9d99e7019db2d739128d7
lucasnahuel <lucas.nahuel@sgoconsultora.com>
2020-09-10 22:31:58 -0300

    vue-bootstrap

--- app-frontend/src/main.js
+++ app-frontend/src/main.js
@@ -1,11 +1,19 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vue from 'vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-createApp(App).use(store).use(router).mount('#app')
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
