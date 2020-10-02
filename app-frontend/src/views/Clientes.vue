9c5f9df705b0f042f7227ccd16f62315e609073b
Ale Diaz <diazale94@outlook.com>
2020-10-01 21:15:56 -0300

    ABM Clientes

--- app-frontend/src/views/Clientes.vue
+++ app-frontend/src/views/Clientes.vue
@@ -21,10 +21,11 @@
                 </div>
             </div>
 
-            <div class="row mt-3">      
-                                          
-                <div class="col-12">
-                    <button class="btn btn-primary mb-3"> Nuevo Cliente </button>
+            <div class="row mt-3">                               
+                <div class="col-12 mb-3">
+                    <button class="btn btn-primary" @click="abrirModalAgregar()"> 
+                        Nuevo Cliente 
+                    </button>
                     <table class="table" id="listado">
                         <thead>
                             <tr>
@@ -44,11 +45,13 @@
                                 <td>{{cliente.telefono}}</td>
                                 <td>{{cliente.domicilio}}</td>
                                 <td>
-                                    <button class="btn btn-info">Editar</button>
-                                        
-
+                                    <button class="btn btn-info" @click="abrirModalEditar(cliente.id)">
+                                    Editar
+                                    </button>
                                     &nbsp;&nbsp;
-                                    <button class="btn btn-danger" v-on:click="eliminarCliente">Eliminar</button>
+                                    <button class="btn btn-danger" @click="abrirModalEliminar(cliente.id)">
+                                    Eliminar
+                                    </button>
                                 </td>
                             </tr>
 
@@ -59,6 +62,49 @@
         </div>
 
     </main>
+
+            <b-modal id="modal_agregar" ref="modal_agregar" hide-footer title="Agregar">
+                <div class="d-block text-center">
+                    <b-form-input v-model="nombre_agregar" placeholder="NOMBRE"></b-form-input>
+                    <b-form-input v-model="apellido_agregar" placeholder="APELLIDO" class="mt-2"></b-form-input>
+                    <b-form-input v-model="telefono_agregar" placeholder="TELEFONO" class="mt-2"></b-form-input>
+                    <b-form-input v-model="domicilio_agregar" placeholder="DOMICILIO" class="mt-2"></b-form-input>
+                </div>
+                <b-button class="mt-2" variant="outline-success" block @click="altaCliente()">Guardar</b-button>
+            </b-modal>
+
+            <b-modal id="modal_editar" ref="modal_editar" hide-footer title="Editar">
+                <div class="d-block text-center">
+                    <b-form-group label="Nombre:" label-for="input-vertical" label-align="left">
+                        <b-form-input v-model="nombre_editar" placeholder="nombre"></b-form-input>
+                    </b-form-group>
+
+                    <b-form-group label="Apellido:" label-for="input-vertical" label-align="left">
+                        <b-form-input v-model="apellido_editar" placeholder="apellido" class="mt-2"></b-form-input>
+                    </b-form-group>
+                    </b-form-group>
+
+                    <b-form-group label="Teléfono:" label-for="input-vertical" label-align="left">
+                        <b-form-input v-model="telefono_editar" placeholder="telefono" class="mt-2"></b-form-input>
+                    </b-form-group>
+
+                    <b-form-group label="Dirección:" label-for="input-vertical" label-align="left">
+                        <b-form-input v-model="domicilio_editar" placeholder="direccion" class="mt-2"></b-form-input>
+                    </b-form-group>
+                </div>
+                <b-button class="mt-2" variant="outline-success" block @click="editarCliente()">Guardar</b-button>
+            </b-modal>
+
+
+            <b-modal id="modal_eliminar" ref="modal_eliminar" hide-footer title="Eliminar">
+                <div class="d-block text-center">
+                    <p>¿Esta seguro desea eliminar el cliente seleccionado?</p>
+                </div>
+                <b-button class="mt-2" variant="outline-danger" block @click="eliminarCliente()">Aceptar</b-button>
+            </b-modal>
+
+
+
 </div>
 </template>
 
@@ -93,10 +139,33 @@ export default {
 
         return {
             clientes: [],
-            nombre: ""
+            
+            nombre_agregar:"",
+            apellido_agregar:"",
+            telefono_agregar:"",
+            domicilio_agregar:"",
+
+            nombre_editar:"",
+            apellido_editar:"",
+            telefono_editar:"",
+            domicilio_editar:"",
+
+            id_a_editar:"",
+            id_a_eliminar:"",
+
         }
     },
     methods: {
+
+        abrirModalAgregar() {
+
+            this.nombre_agregar = "";
+            this.apellido_agregar = "";
+            this.telefono_agregar = "";
+            this.domicilio_agregar = "";
+
+            this.$refs['modal_agregar'].show()
+        },
 
         listaClientes: async function () {
 
@@ -109,38 +178,19 @@ export default {
 
         altaCliente: async function () {
            let params = {
-                nombre: "Pedro",
-                apellido: "Gomez",
-                telefono: "9999999",
-                domicilio: "Urquiza 100",
-            }
-    
+            nombre:this.nombre_agregar,
+            apellido:this.apellido_agregar,
+            telefono:this.telefono_agregar,
+            domicilio:this.domicilio_agregar,
+                
+           }
             let response = await axios.post(API_URL + 'clientes/store', params);
-        },
-
-        modificarCliente: async function () {
-            let params = {
-                id: 1002,
-                nombre: "Pedro EDITADO",
-                apellido: "Gomez EDITADO",
-                telefono: "9999999 EDITADO",
-                domicilio: "Urquiza 100 EDITADO",
-            }
-
-            let response = await axios.post(API_URL + 'clientes/edit', params);
-        },
-
-        obtenerCliente: async function () {
-            let params = {
-                id: 1002,
-            }
-
-            let response = await axios.post(API_URL + 'clientes/get', params);
 
             let respuesta_servidor = response.data;
 
             if (respuesta_servidor.response == true) {
-                alert("EL CLIENTE EXISTE MOSTRAR LOS DATOS EN EL MODAL DE EDICION");
+                this.$refs['modal_agregar'].hide()
+                this.listaClientes();
             } else {
                 let mensajes_errores = "";
 
@@ -152,13 +202,127 @@ export default {
             }
         },
 
-        eliminarCliente: async function () {
+        abrirModalEditar: async function (id_a_editar) {
+
             let params = {
-                id: 101
+                id: id_a_editar,
+            }
+
+            let response = await axios.post(API_URL + 'clientes/get', params);
+
+            let respuesta_servidor = response.data;
+
+            if (respuesta_servidor.response == true) {
+                this.id_a_editar = id_a_editar;
+
+                this.nombre_editar = respuesta_servidor.data.nombre;
+                this.apellido_editar = respuesta_servidor.data.apellido;
+                this.telefono_editar = respuesta_servidor.data.telefono;
+                this.domicilio_editar = respuesta_servidor.data.domicilio;
+
+                this.$refs['modal_editar'].show()
+            } else {
+                let mensajes_errores = "";
+
+                for (let i = 0; i < respuesta_servidor.messages_errors.length; i++) {
+                    mensajes_errores = mensajes_errores + respuesta_servidor.messages_errors[i] + "\n";
+                }
+
+                alert(mensajes_errores);
+            }
+        },
+
+
+        editarCliente: async function () {
+            let params = {
+                id:this.id_a_editar,
+                nombre:this.nombre_editar,
+                apellido:this.apellido_editar,
+                telefono:this.telefono_editar,
+                domicilio:this.domicilio_editar,
+            }
+
+            let response = await axios.post(API_URL + 'clientes/edit', params);
+
+            let respuesta_servidor = response.data;
+
+            if (respuesta_servidor.response == true) {
+                this.$refs['modal_editar'].hide();
+                this.listaClientes();
+            } else {
+                let mensajes_errores = "";
+
+                for (let i = 0; i < respuesta_servidor.messages_errors.length; i++) {
+                    mensajes_errores = mensajes_errores + respuesta_servidor.messages_errors[i] + "\n";
+                }
+
+                alert(mensajes_errores);
+            }
+
+
+            
+        },
+
+        abrirModalEliminar(id_row_seleccionado) {
+
+            this.id_a_eliminar = id_row_seleccionado;
+            this.$refs['modal_eliminar'].show();
+        },
+
+        eliminarCliente: async function () {
+
+            let params = {
+                id: this.id_a_eliminar
             }
 
             let response = await axios.post(API_URL + 'clientes/delete', params);
+
+            let respuesta_servidor = response.data;
+
+            this.$refs['modal_eliminar'].hide();
+
+            if (respuesta_servidor.response == true) {
+                this.listaClientes();
+            } else {
+                let mensajes_errores = "";
+
+                for (let i = 0; i < respuesta_servidor.messages_errors.length; i++) {
+                    mensajes_errores = mensajes_errores + respuesta_servidor.messages_errors[i] + "\n";
+                }
+
+                alert(mensajes_errores);
+            }
+        },
+
+
+//   obtenerCliente: async function () {
+//            let params = {
+//                id: 1002,
+//            }
+//
+//            let response = await axios.post(API_URL + 'clientes/get', params);
+//
+//            let respuesta_servidor = response.data;
+//
+//            if (respuesta_servidor.response == true) {
+//                alert("EL CLIENTE EXISTE MOSTRAR LOS DATOS EN EL MODAL DE EDICION");
+//            } else {
+//                let mensajes_errores = "";
+//
+//                for (let i = 0; i < respuesta_servidor.messages_errors.length; i++) {
+//                    mensajes_errores = mensajes_errores + respuesta_servidor.messages_errors[i] + "\n";
+//                }
+
+//                alert(mensajes_errores);
+//            }
+//        },
+        listaClientes: async function () {
+
+            let params = {}
+            let response = await axios.post(API_URL + 'clientes', params);
+            this.clientes = response.data
         }
+        
     },
     mounted: function () {
         this.listaClientes();
