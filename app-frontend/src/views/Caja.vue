8938b42ee31a3777ec16f470006ac855003946ad
lucasnahuel <lucas.nahuel@sgoconsultora.com>
2020-10-04 02:26:35 -0300

    caja cambios

--- app-frontend/src/views/Caja.vue
+++ app-frontend/src/views/Caja.vue
@@ -1,52 +1,53 @@
 <template>
 <div>
-    <!-- MENU -->
-    <MenuApplicacion></MenuApplicacion>
-    <!-- FIN MENU -->
+    // <!-- MENU -->
+    // <!--<MenuApplicacion></MenuApplicacion>-->
+    // <!-- FIN MENU -->
 
     <main id="main-wrapper">
 
-        <!-- HEADER -->
-        <HeaderApplication></HeaderApplication>
-        <!-- FIN HEADER -->
+        // <!-- HEADER -->
+        // <!--<HeaderApplication></HeaderApplication>-->
+        // <!-- FIN HEADER -->
 
-        <!-- BOTTOM NAV -->
-        <MenuAbajoApplication></MenuAbajoApplication>
-        <!-- FIN BOTTOM NAV -->
+        // <!-- BOTTOM NAV -->
+        // <!--<MenuAbajoApplication></MenuAbajoApplication>-->
+        // <!-- FIN BOTTOM NAV -->
 
         <div class="container" id="contenedor_view">
             <div class="row mt-3">
                 <div class="col-12">
                     <h2 class="title-view">Caja.</h2>
-                    <button class="btn btn-danger float-right" v-on:click=apreta()>Egreso Dinero</button>
-                    <button class="btn btn-danger float-right">Ingreso Dinero</button>
                 </div>
             </div>
 
             <div class="row mt-3">
+                <div class="col-12 mb-3">
+                    <button class="btn btn-primary" @click="abrirModalAgregar()">
+                        Agregar
+                    </button>
+                </div>
                 <div class="col-12">
                     <table class="table" id="listado">
                         <thead>
                             <tr>
                                 <th bgcolor="#FF3F2C"> Id </th>
                                 <th bgcolor="#FF3F2C"> Descripcion </th>
-                                <th bgcolor="#FF3F2C"> Id_Turno </th>
                                 <th bgcolor="#FF3F2C"> Total </th>
                                 <th bgcolor="#FF3F2C"> Fecha </th>
                                 <th bgcolor="#FF3F2C"> Acciones</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for="(caja,index) in caja">
+                            <tr v-for="(caja,index) in Caja">
                                 <td>{{caja.id}}</td>
                                 <td>{{caja.descripcion}}</td>
-                                <td>{{caja.id_turno}}</td>
-                                <td>{{caja.total}}</td>
+                                <td>$ {{caja.total}}</td>
                                 <td>{{caja.fecha}}</td>
                                 <td>
-                                    <button class="btn btn-info">Editar</button>
+                                    <button class="btn btn-info" @click="abrirModalEditar(caja.id)">Editar</button>
                                     &nbsp;&nbsp;
-                                    <button class="btn btn-danger" v-on:click="eliminarCliente">Eliminar</button>
+                                    <button class="btn btn-danger" @click="abrirModalEliminar(caja.id)">Eliminar</button>
                                 </td>
                             </tr>
 
@@ -57,6 +58,46 @@
         </div>
 
     </main>
+
+    <b-modal id="modal_agregar" ref="modal_agregar" hide-footer title="Agregar">
+        <div class="d-block text-center">
+            <b-form-input v-model="caja_id_agregar" placeholder="ID CLIENTE"></b-form-input>
+            <b-form-input v-model="descripcion_agregar" placeholder="DESCRIPCION" class="mt-2"></b-form-input>
+            <b-form-input v-model="total_agregar" placeholder="TOTAL" class="mt-2"></b-form-input>
+            <b-form-input v-model="fecha_agregar" placeholder="FECHA" class="mt-2"></b-form-input>
+        </div>
+        <b-button class="mt-2" variant="outline-success" block @click="agregarCaja()">Guardar</b-button>
+    </b-modal>
+
+    <b-modal id="modal_editar" ref="modal_editar" hide-footer title="Editar">
+        <div class="d-block text-center">
+            <b-form-group label="Caja:" label-for="input-vertical" label-align="left">
+                <b-form-input v-model="caja_id_editar" placeholder="ID CAJA"></b-form-input>
+            </b-form-group>
+
+            <b-form-group label="DESCRIPCION:" label-for="input-vertical" label-align="left">
+                <b-form-input v-model="descripcion_editar" placeholder="DESCRIPCION" class="mt-2"></b-form-input>
+            </b-form-group>
+            </b-form-group>
+
+            <b-form-group label="TOTAL:" label-for="input-vertical" label-align="left">
+                <b-form-input v-model="total_editar" placeholder="TOTAL" class="mt-2"></b-form-input>
+            </b-form-group>
+
+            <b-form-group label="FECHA:" label-for="input-vertical" label-align="left">
+                <b-form-input v-model="fecha_editar" placeholder="FECHA" class="mt-2"></b-form-input>
+            </b-form-group>
+        </div>
+        <b-button class="mt-2" variant="outline-success" block @click="editarCaja()">Guardar</b-button>
+    </b-modal>
+
+    <b-modal id="modal_eliminar" ref="modal_eliminar" hide-footer title="Eliminar">
+        <div class="d-block text-center">
+            <p>¿Esta seguro desea eliminar el registro seleccionado?</p>
+        </div>
+        <b-button class="mt-2" variant="outline-danger" block @click="eliminarCaja()">Aceptar</b-button>
+    </b-modal>
+
 </div>
 </template>
 
@@ -70,8 +111,6 @@ import
 API_URL
 from '@/servicios/Api.js'
 
-console.log(API_URL)
-
 const axios = require('axios');
 
 export default {
@@ -84,83 +123,160 @@ export default {
     data: function () {
 
         return {
-            caja: [],
-            nombre: ""
+            Caja: [],
+
+            caja_id_agregar: "",
+           descripcion_agregar: "",
+            total_agregar: "",
+            fecha_agregar: "",
+
+            caja_id_editar: "",
+            descripcion_editar: "",
+           total_editar: "",
+            fecha_editar: "",
+
+            id_a_editar: "",
+            id_a_eliminar: "",
         }
     },
     methods: {
 
-        // listaCaja: async function () {
+        abrirModalAgregar() {
 
-        //     let params = {}
-        //     let response = await axios.post(API_URL + 'caja', params);
-        //     this.caja = response.data
+            this.caja_id_agregar = "";
+            this.descripcion_agregar = "";
+            this.total_agregar = "";
+            this.fecha_agregar = "";
 
-        //     this.obtenerCaja();
-        // },
+            this.$refs['modal_agregar'].show()
+        },
 
-        // altaCaja: async function () {
-        //     let params = {
-        //         descripcion: "Botella de agua",
-        //         id_turno: "20",
-        //         total: "600",
-        //         fecha: "2020/9/30",
-        //     }
+        agregarCaja: async function () {
 
-        //     let response = await axios.post(API_URL + 'clientes/store', params);
-        // },
+            let params = {
+                caja_id: this.caja_id_agregar,
+                descripcion: this.descripcion_agregar,
+                total: this.total_agregar,
+                fecha: this.fecha_agregar,
+            }
 
-        // modificarCaja: async function () {
-        //     let params = {
-        //         id: 1002,
-        //         descripcion: "Botella de cerveza",
-        //         id_turno: "20",
-        //         total: "700",
-        //         fecha: "2020/9/30",
-        //     }
+            let response = await axios.post(API_URL + 'caja/store', params);
 
-        //     let response = await axios.post(API_URL + 'caja/edit', params);
-        // },
+            let respuesta_servidor = response.data;
 
-        // obtenerCaja: async function () {
-        //     let params = {
-        //         id: 1002,
-        //     }
+            if (respuesta_servidor.response == true) {
+                this.$refs['modal_agregar'].hide()
+                this.listaTurnos();
+            } else {
+                let mensajes_errores = "";
 
-        //     let response = await axios.post(API_URL + 'caja/get', params);
+                for (let i = 0; i < respuesta_servidor.messages_errors.length; i++) {
+                    mensajes_errores = mensajes_errores + respuesta_servidor.messages_errors[i] + "\n";
+                }
 
-        //     let respuesta_servidor = response.data;
+                alert(mensajes_errores);
+            }
+        },
 
-        //     if (respuesta_servidor.response == true) {
-        //         alert("EL CLIENTE EXISTE MOSTRAR LOS DATOS EN EL MODAL DE EDICION");
-        //     } else {
-        //         let mensajes_errores = "";
+        abrirModalEditar: async function (id_a_editar) {
 
-        //         for (let i = 0; i < respuesta_servidor.messages_errors.length; i++) {
-        //             mensajes_errores = mensajes_errores + respuesta_servidor.messages_errors[i] + "\n";
-        //         }
+            let params = {
+                id: id_a_editar,
+            }
 
-        //         alert(mensajes_errores);
-        //     }
-        // },
+            let response = await axios.post(API_URL + 'caja/get', params);
 
-        // eliminarCaja: async function () {
-        //     let params = {
-        //         id: 101
-        //     }
+            let respuesta_servidor = response.data;
 
-        //     let response = await axios.post(API_URL + 'caja/delete', params);
-        // },
-        // apreta: function(){
-        //     console.log("ANDA BIEN")
-            
-        // }
+            if (respuesta_servidor.response == true) {
+                this.id_a_editar = id_a_editar;
+
+                this.caja_id_editar = respuesta_servidor.data.caja_id;
+                this.descripcion_editar = respuesta_servidor.data.descripcion;
+                this.total_editar = respuesta_servidor.data.fecha_desde_esp;
+                this.fecha_hora_hasta_editar = respuesta_servidor.data.fecha_hasta_esp;
+
+                this.$refs['modal_editar'].show()
+            } else {
+                let mensajes_errores = "";
+
+                for (let i = 0; i < respuesta_servidor.messages_errors.length; i++) {
+                    mensajes_errores = mensajes_errores + respuesta_servidor.messages_errors[i] + "\n";
+                }
+
+                alert(mensajes_errores);
+            }
+        },
+
+        editarTurno: async function () {
+
+            let params = {
+                id: this.id_a_editar,
+                cliente_id: this.cliente_id_editar,
+                precio: this.precio_editar,
+                total: this.fecha_hora_desde_editar,
+                fecha: this.fecha_editar,
+            }
+
+            let response = await axios.post(API_URL + 'caja/edit', params);
+
+            let respuesta_servidor = response.data;
+
+            if (respuesta_servidor.response == true) {
+                this.$refs['modal_editar'].hide()
+                this.listaCaja();
+            } else {
+                let mensajes_errores = "";
+
+                for (let i = 0; i < respuesta_servidor.messages_errors.length; i++) {
+                    mensajes_errores = mensajes_errores + respuesta_servidor.messages_errors[i] + "\n";
+                }
+
+                alert(mensajes_errores);
+            }
+        },
+
+        abrirModalEliminar(id_row_seleccionado) {
+
+            this.id_a_eliminar = id_row_seleccionado;
+            this.$refs['modal_eliminar'].show()
+        },
+
+        eliminarTurno: async function () {
+
+            let params = {
+                id: this.id_a_eliminar
+            }
+
+            let response = await axios.post(API_URL + 'caja/delete', params);
+
+            let respuesta_servidor = response.data;
+
+            this.$refs['modal_eliminar'].hide();
+
+            if (respuesta_servidor.response == true) {
+                this.listaCaja();
+            } else {
+                let mensajes_errores = "";
+
+                for (let i = 0; i < respuesta_servidor.messages_errors.length; i++) {
+                    mensajes_errores = mensajes_errores + respuesta_servidor.messages_errors[i] + "\n";
+                }
+
+                alert(mensajes_errores);
+            }
+        },
+
+        listaCaja: async function () {
+
+            let params = {}
+            let response = await axios.post(API_URL + 'caja', params);
+            this.caja = response.data
+        }
     },
-    // mounted: function () {
-    //     this.listaCaja();
-    // }
-
-
-
+    mounted: function () {
+        this.listaCaja();
+    }
 }
 </script>
+
--- server/app/Http/Controllers/Api/CajaController.php
+++ server/app/Http/Controllers/Api/CajaController.php
@@ -3,45 +3,56 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+
 use App\Library\ResponseEstructure;
 
 use App\Caja;
 
-class CobranzasController extends Controller
+class TurnosController extends Controller
 {
-    
     public function index(Request $request)
     {
-        $caja = Caja::all();
+        $caja = DB::table("caja")
+        ->select(
+            "caja.*",
+            "caja.descripcion as caja_descripcion",
+            "caja.total as caja_total",
+            DB::raw("DATE_FORMAT(caja.fecha,'%d/%m/%Y %H:%i') as fecha_caja")
+        )
+        ->leftJoin("caja","caja.id","=","caja.caja_id")
+        ->orderBy("caja.id","desc")
+        ->get();
+
         return response()->json($caja);
     }
 
     public function store(Request $request)
     {
         $response_estructure = new ResponseEstructure();
+        $response_estructure->set_response(false);
+
+        $caja_id = strtolower(trim($request->input("caja_id")));
+        $descripcion = strtolower(trim($request->input("descripcion")));
+        $total = strtolower(trim($request->input("total")));
+        $fecha = strtolower(trim($request->input("fecha")));
+
+        $fecha = \DateTime::createFromFormat("d/m/Y H:i",$fecha);
+        $fecha = $fecha->format("Y-m-d H:i:s");
+
         $response_estructure->set_response(true);
-
-        $descripcion = ucfirst(trim($request->input("descripcion")));
-        // $id_turno = strtolower(trim($request->input("id_turno")));
-        // $total = strtolower(trim($request->input("total")));
-        // $fecha = strtolower(trim($request->input("fecha")));
-
-        if(strlen($descripcion) < 3)
-        {
-            $response_estructure->set_response(false);
-            $response_estructure->add_message_error("La descripcion debe tener 3 caracteres como minimo");
-        }
-
 
         if($response_estructure->get_response() == true)
         {
-            $caja_new = new Caja();
-            $caja_new->descripcion = $descripcion;
-            $caja_new->id_turno = $id_turno;
-            $caja_new->total = $total;
-            $caja_new->fecha = $fecha;
-            $caja_new->save();
+            $caja_row = new Caja();
+            $caja_row->caja_id = $caja_id ;
+            $caja_row->descripcion = $descripcion;
+            $caja_row->total = $total;
+            $caja_row->fecha = $fecha;
+            $turno_row->save();
+
+            $response_estructure->set_response(true);
         }
 
         return response()->json($response_estructure->get_response_array());
@@ -49,7 +60,8 @@ class CobranzasController extends Controller
 
     public function edit(Request $request)
     {
-        $respuesta = false;
+        $response_estructure = new ResponseEstructure();
+        $response_estructure->set_response(false);
 
         $id = $request->input("id");
 
@@ -57,21 +69,24 @@ class CobranzasController extends Controller
 
         if($caja_row)
         {
-            $descripcion = ucfirst(trim($request->input("descripcion")));
-            $id_turno = strtolower(trim($request->input("id_turno")));
+            $caja_id = strtolower(trim($request->input("caja_id")));
+            $descripcion = strtolower(trim($request->input("descripcion")));
             $total = strtolower(trim($request->input("total")));
             $fecha = strtolower(trim($request->input("fecha")));
 
-            $cliente_row->descripcion = $descripcion;
-            $cliente_row->id_turno = $id_turno;
-            $cliente_row->total = $total;
-            $cliente_row->fecha = $fecha;
-            $cliente_row->save();
+            $fecha = \DateTime::createFromFormat("d/m/Y H:i",$fecha);
+            $fecha = $fecha->format("Y-m-d H:i:s");
 
-            $respuesta = true;
+            $caja_row->caja_id = $caja_id ;
+            $caja_row->descripcion = $descripcion;
+            $caja_row->total= $total;
+            $caja_row->fecha= $fecha;
+            $caja_row->save();
+
+            $response_estructure->set_response(true);
         }
 
-        return response()->json($respuesta);
+        return response()->json($response_estructure->get_response_array());
     }
 
     public function get(Request $request)
@@ -81,10 +96,20 @@ class CobranzasController extends Controller
 
         $id = $request->input("id");
 
-        $caja_row = Caja::where("id",$id)->first();
+        $caja_row = $caja = DB::table("caja")
+        ->select(
+            "caja.*",
+            "caja.descripcion as caja_descripcion",
+            "caja.total as caja_total",
+            DB::raw("DATE_FORMAT(caja.fecha,'%d/%m/%Y %H:%i') as fecha")
+        )
+        ->leftJoin("caja","caja.id","=","caja.cliente_id")
+        ->where("caja.id",$id)
+        ->get();
 
         if($caja_row)
         {
+            $caja_row = $caja_row[0];
             $response_estructure->set_data($caja_row);
         }
         else
@@ -98,7 +123,8 @@ class CobranzasController extends Controller
 
     public function delete(Request $request)
     {
-        $respuesta = false;
+        $response_estructure = new ResponseEstructure();
+        $response_estructure->set_response(false);
 
         $id = $request->input("id");
 
@@ -107,9 +133,14 @@ class CobranzasController extends Controller
         if($caja_row)
         {
             $caja_row->delete();
-            $respuesta = true;
+            $response_estructure->set_response(true);
         }
-        
-        return response()->json($respuesta);
+        else
+        {
+            $response_estructure->set_response(false);
+            $response_estructure->add_message_error("La caja no existe");
+        }
+
+        return response()->json($response_estructure->get_response_array());
     }
 }
