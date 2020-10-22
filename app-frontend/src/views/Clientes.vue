8c58dfdd286c1f109410e51a23415321aa341c5a
Mario Olivera <mario.olivera96@gmail.com>
2020-10-21 22:25:16 -0300

    fix

--- app-frontend/src/views/Clientes.vue
+++ app-frontend/src/views/Clientes.vue
@@ -7,11 +7,11 @@
     <main id="main-wrapper">
 
         <!-- HEADER -->
-    <!--     <HeaderApplication></HeaderApplication>
+        <!--     <HeaderApplication></HeaderApplication>
          <!-- FIN HEADER -->
 
-         <!-- BOTTOM NAV -->
-    <!--     <MenuAbajoApplication></MenuAbajoApplication>
+        <!-- BOTTOM NAV -->
+        <!--     <MenuAbajoApplication></MenuAbajoApplication>
          <!-- FIN BOTTOM NAV -->
 
         <div class="container" id="contenedor_view">
@@ -21,13 +21,13 @@
                 </div>
             </div>
 
-            <div class="row mt-3">                               
+            <div class="row mt-3">
                 <div class="col-12 mb-3">
-                    <button class="btn btn-primary" @click="abrirModalAgregar()"> 
-                        Nuevo Cliente 
+                    <button class="btn btn-primary" @click="abrirModalAgregar()">
+                        Nuevo Cliente
                     </button>
                     <br>
-                    <input type="text"  placeholder="Buscar Cliente..." v-model="busqueda" >
+                    <input type="text" placeholder="Buscar Cliente..." v-model="busqueda">
                     <button class="btn-btn primary" @click="listaClientes">Buscar</button>
                     <table class="table" id="listado">
                         <thead>
@@ -49,11 +49,11 @@
                                 <td>{{cliente.domicilio}}</td>
                                 <td>
                                     <button class="btn btn-info" @click="abrirModalEditar(cliente.id)">
-                                    Editar
+                                        Editar
                                     </button>
                                     &nbsp;&nbsp;
                                     <button class="btn btn-danger" @click="abrirModalEliminar(cliente.id)">
-                                    Eliminar
+                                        Eliminar
                                     </button>
                                 </td>
                             </tr>
@@ -66,58 +66,50 @@
 
     </main>
 
-            <b-modal id="modal_agregar" ref="modal_agregar" hide-footer title="Agregar">
-                <div class="d-block text-center">
-                    <b-form-input v-model="nombre_agregar" placeholder="NOMBRE"></b-form-input>
-                    <b-form-input v-model="apellido_agregar" placeholder="APELLIDO" class="mt-2"></b-form-input>
-                    <b-form-input v-model="telefono_agregar" placeholder="TELEFONO" class="mt-2"></b-form-input>
-                    <b-form-input v-model="domicilio_agregar" placeholder="DOMICILIO" class="mt-2"></b-form-input>
-                </div>
-                <b-button class="mt-2" variant="outline-success" block @click="altaCliente()">Guardar</b-button>
-            </b-modal>
+    <b-modal id="modal_agregar" ref="modal_agregar" hide-footer title="Agregar">
+        <div class="d-block text-center">
+            <b-form-input v-model="nombre_agregar" placeholder="NOMBRE"></b-form-input>
+            <b-form-input v-model="apellido_agregar" placeholder="APELLIDO" class="mt-2"></b-form-input>
+            <b-form-input v-model="telefono_agregar" placeholder="TELEFONO" class="mt-2"></b-form-input>
+            <b-form-input v-model="domicilio_agregar" placeholder="DOMICILIO" class="mt-2"></b-form-input>
+        </div>
+        <b-button class="mt-2" variant="outline-success" block @click="altaCliente()">Guardar</b-button>
+    </b-modal>
 
-            <b-modal id="modal_editar" ref="modal_editar" hide-footer title="Editar">
-                <div class="d-block text-center">
-                    <b-form-group label="Nombre:" label-for="input-vertical" label-align="left">
-                        <b-form-input v-model="nombre_editar" placeholder="nombre"></b-form-input>
-                    </b-form-group>
+    <b-modal id="modal_editar" ref="modal_editar" hide-footer title="Editar">
+        <div class="d-block text-center">
+            <b-form-group label="Nombre:" label-for="input-vertical" label-align="left">
+                <b-form-input v-model="nombre_editar" placeholder="nombre"></b-form-input>
+            </b-form-group>
 
-                    <b-form-group label="Apellido:" label-for="input-vertical" label-align="left">
-                        <b-form-input v-model="apellido_editar" placeholder="apellido" class="mt-2"></b-form-input>
-                    </b-form-group>
-                    </b-form-group>
+            <b-form-group label="Apellido:" label-for="input-vertical" label-align="left">
+                <b-form-input v-model="apellido_editar" placeholder="apellido" class="mt-2"></b-form-input>
+            </b-form-group>
+            </b-form-group>
 
-                    <b-form-group label="Teléfono:" label-for="input-vertical" label-align="left">
-                        <b-form-input v-model="telefono_editar" placeholder="telefono" class="mt-2"></b-form-input>
-                    </b-form-group>
+            <b-form-group label="Teléfono:" label-for="input-vertical" label-align="left">
+                <b-form-input v-model="telefono_editar" placeholder="telefono" class="mt-2"></b-form-input>
+            </b-form-group>
 
-                    <b-form-group label="Dirección:" label-for="input-vertical" label-align="left">
-                        <b-form-input v-model="domicilio_editar" placeholder="direccion" class="mt-2"></b-form-input>
-                    </b-form-group>
-                </div>
-                <b-button class="mt-2" variant="outline-success" block @click="editarCliente()">Guardar</b-button>
-            </b-modal>
+            <b-form-group label="Dirección:" label-for="input-vertical" label-align="left">
+                <b-form-input v-model="domicilio_editar" placeholder="direccion" class="mt-2"></b-form-input>
+            </b-form-group>
+        </div>
+        <b-button class="mt-2" variant="outline-success" block @click="editarCliente()">Guardar</b-button>
+    </b-modal>
 
-
-            <b-modal id="modal_eliminar" ref="modal_eliminar" hide-footer title="Eliminar">
-                <div class="d-block text-center">
-                    <p>¿Esta seguro desea eliminar el cliente seleccionado?</p>
-                </div>
-                <b-button class="mt-2" variant="outline-danger" block @click="eliminarCliente()">Aceptar</b-button>
-            </b-modal>
-
-
+    <b-modal id="modal_eliminar" ref="modal_eliminar" hide-footer title="Eliminar">
+        <div class="d-block text-center">
+            <p>¿Esta seguro desea eliminar el cliente seleccionado?</p>
+        </div>
+        <b-button class="mt-2" variant="outline-danger" block @click="eliminarCliente()">Aceptar</b-button>
+    </b-modal>
 
 </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
-
-
-
-
 
 import MenuApplicacion from '@/components/template/MenuApplicacion.vue'
 import HeaderApplication from '@/components/template/HeaderApplication.vue'
@@ -142,21 +134,21 @@ export default {
 
         return {
             clientes: [],
-            
-            nombre_agregar:"",
-            apellido_agregar:"",
-            telefono_agregar:"",
-            domicilio_agregar:"",
 
-            nombre_editar:"",
-            apellido_editar:"",
-            telefono_editar:"",
-            domicilio_editar:"",
+            nombre_agregar: "",
+            apellido_agregar: "",
+            telefono_agregar: "",
+            domicilio_agregar: "",
 
-            id_a_editar:"",
-            id_a_eliminar:"",
-            nombre_filtrar:"",
-            busqueda:"",
+            nombre_editar: "",
+            apellido_editar: "",
+            telefono_editar: "",
+            domicilio_editar: "",
+
+            id_a_editar: "",
+            id_a_eliminar: "",
+            nombre_filtrar: "",
+            busqueda: "",
 
         }
     },
@@ -175,8 +167,11 @@ export default {
         listaClientes: async function () {
 
             let params = {
-                busqueda:this.busqueda
+                busqueda: this.busqueda
             }
+
+            console.log(params)
+
             let response = await axios.post(API_URL + 'clientes', params);
             this.clientes = response.data
 
@@ -184,13 +179,13 @@ export default {
         },
 
         altaCliente: async function () {
-           let params = {
-            nombre:this.nombre_agregar,
-            apellido:this.apellido_agregar,
-            telefono:this.telefono_agregar,
-            domicilio:this.domicilio_agregar,
-                
-           }
+            let params = {
+                nombre: this.nombre_agregar,
+                apellido: this.apellido_agregar,
+                telefono: this.telefono_agregar,
+                domicilio: this.domicilio_agregar,
+
+            }
             let response = await axios.post(API_URL + 'clientes/store', params);
 
             let respuesta_servidor = response.data;
@@ -239,14 +234,13 @@ export default {
             }
         },
 
-
         editarCliente: async function () {
             let params = {
-                id:this.id_a_editar,
-                nombre:this.nombre_editar,
-                apellido:this.apellido_editar,
-                telefono:this.telefono_editar,
-                domicilio:this.domicilio_editar,
+                id: this.id_a_editar,
+                nombre: this.nombre_editar,
+                apellido: this.apellido_editar,
+                telefono: this.telefono_editar,
+                domicilio: this.domicilio_editar,
             }
 
             let response = await axios.post(API_URL + 'clientes/edit', params);
@@ -266,8 +260,6 @@ export default {
                 alert(mensajes_errores);
             }
 
-
-            
         },
 
         abrirModalEliminar(id_row_seleccionado) {
@@ -301,42 +293,32 @@ export default {
             }
         },
 
+        //   obtenerCliente: async function () {
+        //            let params = {
+        //                id: 1002,
+        //            }
+        //
+        //            let response = await axios.post(API_URL + 'clientes/get', params);
+        //
+        //            let respuesta_servidor = response.data;
+        //
+        //            if (respuesta_servidor.response == true) {
+        //                alert("EL CLIENTE EXISTE MOSTRAR LOS DATOS EN EL MODAL DE EDICION");
+        //            } else {
+        //                let mensajes_errores = "";
+        //
+        //                for (let i = 0; i < respuesta_servidor.messages_errors.length; i++) {
+        //                    mensajes_errores = mensajes_errores + respuesta_servidor.messages_errors[i] + "\n";
+        //                }
 
-//   obtenerCliente: async function () {
-//            let params = {
-//                id: 1002,
-//            }
-//
-//            let response = await axios.post(API_URL + 'clientes/get', params);
-//
-//            let respuesta_servidor = response.data;
-//
-//            if (respuesta_servidor.response == true) {
-//                alert("EL CLIENTE EXISTE MOSTRAR LOS DATOS EN EL MODAL DE EDICION");
-//            } else {
-//                let mensajes_errores = "";
-//
-//                for (let i = 0; i < respuesta_servidor.messages_errors.length; i++) {
-//                    mensajes_errores = mensajes_errores + respuesta_servidor.messages_errors[i] + "\n";
-//                }
+        //                alert(mensajes_errores);
+        //            }
+        //        },
 
-//                alert(mensajes_errores);
-//            }
-//        },
-        listaClientes: async function () {
-
-            let params = {}
-            let response = await axios.post(API_URL + 'clientes', params);
-            this.clientes = response.data
-        }
-        
     },
 
-    
     mounted: function () {
         this.listaClientes();
     }
 }
-
-
 </script>
